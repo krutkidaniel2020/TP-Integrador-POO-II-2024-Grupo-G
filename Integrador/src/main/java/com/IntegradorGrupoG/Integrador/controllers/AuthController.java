fbacc196f62f7d6b54cbb0853a09d458be2d8b0a
krutkidaniel2020 <krutki.daniel2020@gmail.com>
2024-11-28 23:07:15 -0300

    Quitando javascript, modificando pom

--- Integrador/src/main/java/com/IntegradorGrupoG/Integrador/controllers/AuthController.java
+++ Integrador/src/main/java/com/IntegradorGrupoG/Integrador/controllers/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Controlador de autenticacion
  */
-@RestController
+@RestController //@Controller
 public class AuthController {
 
     /**
